445bdd788b05ca40290e9f524d973f286b05ab8f
Takeru Gorai <tgorai@yahoo-corp.jp>
2023-12-28 13:08:40 +0900

    README.md追加

--- whisper_server.py
+++ whisper_server.py
@@ -1,7 +1,6 @@
 import os
 import time
 import re
-from turtle import pen
 from flask import Flask, request, redirect
 import whisper
 import threading
@@ -10,11 +9,14 @@ from requests.exceptions import Timeout
 
 openai.api_key = open("api_key").read().strip()
 
+PORT = 9999
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'m4a','mp3','wav'}
-USE_API = True
+USE_WHISPER_API = True
 WHISPER_MODEL_NAME = 'small' # tiny, base, small, medium
+WHISPER_API_MODEL_NAME = 'whisper-1'
 WHISPER_DEVICE = 'cpu' # cpu, cuda
+CHATGPT_MODEL_NAME = 'gpt-3.5-turbo-0613'
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app = Flask(__name__, static_url_path='/')
@@ -22,11 +24,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 lock = threading.Lock()
 
-if not USE_API:
+if not USE_WHISPER_API:
    print('loading whisper model', WHISPER_MODEL_NAME, WHISPER_DEVICE)
    whisper_model = whisper.load_model(WHISPER_MODEL_NAME, device=WHISPER_DEVICE)
 prev_transcript = "" # whisper API使用時にpromptとして前回のtranscriptionを入れる
-pending_translation = "" # APIのtimeout等で翻訳をskipしたテキストを保持しておく
+pended_translation = "" # APIのtimeout等で翻訳をskipしたテキストを保持しておく
 
 
 @app.route('/')
@@ -64,11 +66,11 @@ def transcribe():
 def trascribe_by_whisper(saved_filename):
     time_sta = time.perf_counter()
     print('start transcribe ' + str(time_sta))
-    if USE_API:
+    if USE_WHISPER_API:
         audio_file = open(saved_filename, "rb")
-        # TODO: prompt使うかどうかをargparseで指定できるようにする
+        # 精度を上げるため前回のtranscriptionをpromptとして渡す
         global prev_transcript
-        result = openai.Audio.transcribe("whisper-1", audio_file, prompt=prev_transcript, language='en')
+        result = openai.Audio.transcribe(WHISPER_API_MODEL_NAME, audio_file, prompt=prev_transcript, language='en')
         prev_transcript = result.text
         print(f"whisper API responce: '{result.text}'")
     else:
@@ -80,7 +82,7 @@ def trascribe_by_whisper(saved_filename):
 
 
 def translate_by_chatgpt(transcription):
-    global pending_translation
+    global pended_translation
 
     to_translate = extract_texts_to_translate(transcription)
     if to_translate == "":
@@ -89,12 +91,12 @@ def translate_by_chatgpt(transcription):
     print(f"started to translate '{to_translate}'")
 
     prompt = f"""
-        以下は自然言語処理の国際学会での英語のスピーチの一部です。日本語に翻訳したものだけを出力ください
+        以下は自然言語処理の国際学会での英語のスピーチの一部です。日本語に翻訳したものだけを出力してください
         {to_translate}
     """
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0613",
+            model=CHATGPT_MODEL_NAME,
             messages=[
                 {"role": "system", "content": "あなたは翻訳機です"},
                 {"role": "user", "content": prompt},
@@ -104,7 +106,7 @@ def translate_by_chatgpt(transcription):
         )
     except Timeout as e:
         print(e)
-        pending_translation = f"{to_translate} {pending_translation}"
+        pended_translation = f"{to_translate} {pended_translation}"
         return ""
     except Exception as e:
         raise e
@@ -116,9 +118,9 @@ def translate_by_chatgpt(transcription):
 
 def extract_texts_to_translate(transcription):
     """
-    transcriptionが文の途中で終わっている場合, 文が完結している部分だけ翻訳し途中の文はpending_translationに保持
+    transcriptionのうち文が完結している部分だけ抽出し残りはpending_translationに保持
     """
-    global pending_translation
+    global pended_translation
     # 文等の空白を削除
     transcription = re.sub(r'^\s+', '', transcription)
     # 文の途中で終わっている場合末尾に...がつくことがあるので削除
@@ -128,21 +130,21 @@ def extract_texts_to_translate(transcription):
     # transcriptionの先頭が大文字で始まっていない場合は文の終わりでないとみなしpending_translationの末尾の句読点を削除
     eos_markers = ['.', '?', '!']
     starts_with_upper = transcription[0].isupper()
-    pended_ends_with_eos = any(pending_translation.endswith(marker) for marker in eos_markers)
+    pended_ends_with_eos = any(pended_translation.endswith(marker) for marker in eos_markers)
     if pended_ends_with_eos and not starts_with_upper:
-        pending_translation = pending_translation[:-1]
+        pended_translation = pended_translation[:-1]
     
-    whole_texts = f"{pending_translation} {transcription}"
+    whole_texts = f"{pended_translation} {transcription}"
 
     if re.search(r'[\.\?\!]\s+(?=\S)', whole_texts):
-        pending_translation = re.split(r'[\.\?\!]\s+(?=\S)', whole_texts)[-1]
-        to_translate = whole_texts[:-len(pending_translation)]
+        pended_translation = re.split(r'[\.\?\!]\s+(?=\S)', whole_texts)[-1]
+        to_translate = whole_texts[:-len(pended_translation)]
     else:
-        pending_translation = whole_texts
+        pended_translation = whole_texts
         to_translate = ""
 
     return to_translate
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=9000)
+    app.run(host='localhost', port=PORT)
